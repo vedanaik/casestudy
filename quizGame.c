448ee670b95426ffdfea68bc367c05fb93bf9ebe
neelthegreat <neelnemade12345@gmail.com>
2025-01-13 18:06:58 +0530

    Add files via upload

---
+++ quizGame.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+int main(){
+    char choice, S, Q;
+    int rups;
+    int ans1,ans2,ans3,ans4,ans5;
+    
+    printf("Welcome to KBC\n");
+    printf("!!Press S to start the game!!\n");
+    printf("!!Press Q to quit the game!!\n");
+    scanf("%c",&choice);
+    if(choice=='S'){
+        printf("Q)1 What is the capital of France?\n\n");
+        printf("1. Berlin\n");
+        printf("2. Madrid\n");
+        printf("3. Paris\n");
+        printf("4. Rome\n");
+        printf("Enter Your Answer : ");
+        scanf("%d",&ans1);
+        if(ans1==3){
+            printf("Correct Answer\n");
+            rups = 100;
+            printf("You have earned rupees %d\n", rups);
+        } else{
+            printf("Wrong Answer\n");
+            printf("You have not earned\n");
+        }
+        printf("Q)2 Which country is known as the 'Land of the rising sun?\n\n");
+        printf("1. India\n");
+        printf("2. Japan\n");
+        printf("3. Nepal\n");
+        printf("4. China\n");
+        printf("Enter Your Answer : ");
+        scanf("%d",&ans2);
+        if(ans2==2){
+            printf("Correct Answer\n");
+            rups += 100;
+            printf("You have earned rupees %d\n", rups);
+        }
+         else{
+            printf("Wrong Answer\n");
+            printf("You have not earned\n");
+        }
+        printf("Q)1 From which language did the word 'Ketchup' come from?\n\n");
+        printf("1. French\n");
+        printf("2. Spanish\n");
+        printf("3. Chinese\n");
+        printf("4. Japanese\n");
+        printf("Enter Your Answer : ");
+        scanf("%d",&ans3);
+        if(ans3==4){
+            printf("Correct Answer\n");
+            rups += 500;
+            printf("You have earned rupees %d\n", rups);
+        }
+         else{
+            printf("Wrong Answer\n");
+            printf("You have not earned\n");
+        }
+       
+        printf("Q)2 What is the capital city of Canada?\n\n");
+        printf("1. Toronto\n");
+        printf("2. Ottawa\n");
+        printf("3. Vancouver\n");
+        printf("4. Florida\n");
+        printf("Enter Your Answer : ");
+        scanf("%d",&ans2);
+        if(ans2==2){
+            printf("Correct Answer\n");
+            rups += 100;
+            printf("You have earned rupees %d\n", rups);
+        }
+        else{
+            printf("Wrong Answer\n");
+            printf("You have not earned\n");
+        }
+        
+    }
+    else{
+        printf("The game has ended\n");
+    }
+    return 0;
+}
